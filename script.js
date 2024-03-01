f389a935a687ed72e83c91110f9fad446e5caec6
Lawson Tuladhar <lawsontuladhar@gmail.com>
2024-03-02 00:30:28 +0545

    Final update to the codes

--- script.js
+++ script.js
@@ -1,69 +1,115 @@
+const quotes = {};
+
+fetch("quotes.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((quote) => {
+      if (!quotes[quote.category]) {
+        quotes[quote.category] = [];
+      }
+      quotes[quote.category].push({
+        text: quote.quote,
+        author: quote.author,
+        category: quote.category
+      });
+    });
+    displayQuote();
+  })
+  .catch((error) => console.error("Error loading quotes:", error));
+
 let currentCategory = "happiness";
 let currentQuoteIndex = 0;
-let fontSize = 16;
-
-console.log("hello");
-
-function fetchData() {
-  fetch("./quotes.json")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.error("Something went wrong with retrieving the quote!");
-      console.error(err);
-    });
-}
-
-const quote = fetchData();
-
-// console.log(quote);
+let darkMode = false;
+let fontSize = 22;
+let categoryQuotes;
 
 function displayQuote() {
-  const quoteElement = document.getElementById("quote");
-  const authorElement = document.getElementById("author");
-  const categoryElement = (document.getElementById("category").innerHTML =
-    "Category");
-  const categoryQuotes = quote[currentCategory];
+  const quoteContainer = document.getElementById("quoteContainer");
+  const quoteText = document.getElementById("quoteText");
+  const quoteAuthor = document.getElementById("quoteAuthor");
+  const quoteCategory = document.getElementById("quoteCategory");
+
+  categoryQuotes = quotes[currentCategory];
+
+  const currentQuote = categoryQuotes[currentQuoteIndex];
+
+  quoteText.innerHTML = `<p>"${currentQuote.text}"</p>`;
+  quoteAuthor.innerHTML = `<p>- ${currentQuote.author}</p>`;
+  quoteContainer.style.fontSize = `${fontSize}px`;
+  quoteCategory.innerHTML = `<p>"${currentQuote.category}"</p>`;
+  quoteContainer.style.fontSize = `${fontSize}px`;
+
+  if (darkMode) {
+    quoteGenerator.classList.add("dark-mode");
+    container.classList.add("dark-mode1");
+    quoteContainer.classList.add("dark-mode2");
+    fontControls.classList.add("dark-mode2");
+  } else {
+    quoteGenerator.classList.remove("dark-mode");
+    container.classList.remove("dark-mode1");
+    quoteContainer.classList.remove("dark-mode2");
+    fontControls.classList.remove("dark-mode2");
+  }
 }
 
 function nextQuote() {
-  currentQuoteIndex = (currentQuoteIndex + 1) % quote[currentCategory].length;
-  displayQuote;
+  document.getElementById("prevButton").removeAttribute("disabled");
+  currentQuoteIndex++;
+  if (currentQuoteIndex === categoryQuotes.length - 1) {
+    document.getElementById("nextButton").setAttribute("disabled", "");
+  }
+
+  displayQuote();
 }
 
 function prevQuote() {
-  currentQuoteIndex =
-    (currentQuoteIndex - 1 + quote[currentCategory].length) %
-    quote[currentCategory].length;
+  document.getElementById("nextButton").removeAttribute("disabled");
+  if (currentQuoteIndex === 0) {
+    document.getElementById("prevButton").setAttribute("disabled", "");
+    return;
+  }
+  currentQuoteIndex--;
   displayQuote();
 }
 
 function randomQuote() {
-  currentQuoteIndex = Math.floor(Math.random() * quote[currentCategory].length);
-  displayQuote();
+  const categories = Object.keys(quotes);
+  const randomCategory =
+    categories[Math.floor(Math.random() * categories.length)];
+  const categoryQuotes = quotes[randomCategory];
+
+  if (categoryQuotes) {
+    currentCategory = randomCategory;
+    currentQuoteIndex = Math.floor(Math.random() * categoryQuotes.length);
+    displayQuote();
+  }
 }
 
 function changeCategory() {
-  currentCategory = document.getElementById("category").ariaValueMax;
+  currentCategory = document.getElementById("category").value;
   currentQuoteIndex = 0;
   displayQuote();
 }
 
 function toggleDarkMode() {
-  const body = document.body;
-  body.classList.toggle("dark-mode");
+  darkMode = !darkMode;
+  displayQuote();
 }
 
 function increaseFontSize() {
   fontSize += 2;
+  document.getElementById("decreaseFontSize").removeAttribute("disabled");
+  if (fontSize === 36) {
+    document.getElementById("increaseFontSize").setAttribute("disabled", "");
+  }
   displayQuote();
 }
 
 function decreaseFontSize() {
+  document.getElementById("increaseFontSize").removeAttribute("disabled");
   fontSize = Math.max(10, fontSize - 2);
+  if (fontSize === 10) {
+    document.getElementById("decreaseFontSize").setAttribute("disabled", "");
+  }
   displayQuote();
 }
-
-// displayQuote();
